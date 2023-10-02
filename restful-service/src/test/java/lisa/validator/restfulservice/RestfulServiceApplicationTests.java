eb0e405834cee760d021d00b1dd05157e91edff3
mahirgamal <86919381+mahirgamal@users.noreply.github.com>
2023-10-02 13:42:35 +1100

    init

---
+++ restful-service/src/test/java/lisa/validator/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lisa.validator.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
